f6ecd2135e27e8969d82e15765d96b2360825e15
kmw2378 <rlarla677@gmail.com>
2025-01-16 14:04:58 +0900

    [FEAT] 실행 파일 추가

---
+++ src/test/java/ject/componote_notification/ComponoteApplicationTests.java
@@ -0,0 +1,13 @@
+package ject.componote_notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponoteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
